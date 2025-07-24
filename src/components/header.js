3a4d615264a9f139181ce9a2e7e91121044f9363
Your GitHub Username <youremail@example.com>
2025-07-24 15:10:16 +0300

    Added Import

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,19 @@
-const headerNav =[
+import React, { useRef, useEffect } from 'react';
+
+import { Link, useLocation } from 'react-router-dom';
+
+import './header.scss';
+
+import logo from '../../assets/tmovie.png';
+
+const headerNav = [
     {
-        display:'Home',
+        display: 'Home',
         path: '/'
     },
     {
         display: 'Movies',
-        path: '/movies'
+        path: '/movie'
     },
     {
         display: 'TV Series',
@@ -13,48 +21,48 @@ const headerNav =[
     }
 ];
 
-const Header =() => {
-    const {pathname} = useLocation();
-    const headerRef =useRef(null);
+const Header = () => {
 
-    const active =headerNav.findIndex(e => e.path ===pathname);
+    const { pathname } = useLocation();
+    const headerRef = useRef(null);
+
+    const active = headerNav.findIndex(e => e.path === pathname);
 
     useEffect(() => {
-        const shrinkHeader = () =>{
-            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ){
-                headerRef.current.classList.remove ('shrink');
-            }else {
+        const shrinkHeader = () => {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
                 headerRef.current.classList.add('shrink');
+            } else {
+                headerRef.current.classList.remove('shrink');
             }
-            }
-            window.addEventListener('scroll', shrinkHeader);
+        }
+        window.addEventListener('scroll', shrinkHeader);
         return () => {
             window.removeEventListener('scroll', shrinkHeader);
-        
         };
+    }, []);
 
-
-
-    },[]
-);
-    return(
-        <div ref ={headerRef} className="header">
-            <div className ="header __wrap container">
-                <div className ="logo">
-                    <link to ="/">Movies</link>
-            </div>
-          
-            <ul className ="header__nav">
-                {
-                    headerNav.map((item, index) => (
-                        <li key={index} className={`header__nav-item ${index === active ? 'active' : ''}`}>
-                            <Link to={item.path}>{item.display}</Link>
-                        </li>
-                    ))  
-                }
+    return (
+        <div ref={headerRef} className="header">
+            <div className="header__wrap container">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <Link to="/">tMovies</Link>
+                </div>
+                <ul className="header__nav">
+                    {
+                        headerNav.map((e, i) => (
+                            <li key={i} className={`${i === active ? 'active' : ''}`}>
+                                <Link to={e.path}>
+                                    {e.display}
+                                </Link>
+                            </li>
+                        ))
+                    }
                 </ul>
-        </div>
+            </div>
         </div>
     );
 }
+
 export default Header;
