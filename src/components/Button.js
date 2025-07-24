4acbf073ea30e6dd2ae02ed63db627af96b843be
Abdullahi Omar <abdullahiomar58@icloud.com>
2025-07-24 12:32:34 +0300

    added button

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+const Button = props => {
+    return (
+        <button
+            className={`btn ${props.className}`}
+            onClick={props.onClick ? props.onClick : null}
+        >
+            {props.children}
+        </button>
+    );
+}
+Button.propTypes={
+    onClick: PropTypes.func
+}
+export default Button;
